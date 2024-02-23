e7cb1a1d71f4b314970df3bc8b5a1006c84f0a4b
gre1wy <grundayaroslav28@gmail.com>
2024-02-23 13:56:17 +0200

    mb compl

--- lab1.py
+++ lab1.py
@@ -1,17 +1,21 @@
 #З використанням рекурсії розв’язати наступні задачі.
 #1. Піднести до додатного цілого степеня дійсне ненульове число.
 def power(x, n):
+
     if x <= 0:
         raise ValueError("x should be positive")
+    if not isinstance(x, int):
+        raise ValueError("x should be integer")
     if n < 0:
         raise ValueError("n should be non-negative")
+    
     # Базовий випадок: x^0 = 1
     if n == 0:
         return 1
     # Рекурсивний випадок: x^n = x * x^(n-1)
     else:
         return x * power(x, n-1)
-    
+
 import unittest
 
 class TestPowerFunction(unittest.TestCase):
@@ -26,16 +30,20 @@ class TestPowerFunction(unittest.TestCase):
         self.assertEqual(power(1, 0), 1)
 
     def test_power_with_non_positive_base(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaisesRegex(ValueError, "x should be positive"):
             power(0, 3)
 
-        with self.assertRaises(ValueError):
+        with self.assertRaisesRegex(ValueError, "x should be positive"):
             power(-2, 4)
-                
-    def test_power_with_negative_base_and_even_exponent(self):
-        with self.assertRaises(ValueError):
-            power(-3, 2)
+            
+    def test_power_with_non_integer_base(self):
+        with self.assertRaisesRegex(ValueError, "x should be integer"):
+            power(1.5, 3)
 
-print("something t43")
+    def test_power_with_negative_exponent(self):
+        with self.assertRaisesRegex(ValueError, "n should be non-negative"):
+            power(2, -3)
+
 if __name__ == '__main__':
     unittest.main()
+    
