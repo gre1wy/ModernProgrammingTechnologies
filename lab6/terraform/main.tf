36a4331ac36229dae6f9a797fabdcfc674b19ce0
gre1wy <grundayaroslav28@gmail.com>
2024-05-28 00:12:07 +0300

    aaaaaaaaaaaaaaaaaaaaa

--- lab6/terraform/main.tf
+++ lab6/terraform/main.tf
@@ -74,3 +74,13 @@ resource "aws_s3_bucket_lifecycle_configuration" "s3_start_lifecycle" {
   }
   
 }
+resource "aws_s3_bucket_notification" "s3_start_notification" {
+  bucket = aws_s3_bucket.s3_start.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.test_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = ""
+    filter_suffix       = ""
+  }
+}
